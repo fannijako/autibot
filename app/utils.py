0da289c91028f3e3cbc0800797ecd53a1d1916e7
Fanni Jakó <40933785+fannijako@users.noreply.github.com>
2024-11-02 14:27:40 +0100

    utils

---
+++ app/utils.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+
+def load_env():
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    load_dotenv(os.path.join(project_root, '.env'))
